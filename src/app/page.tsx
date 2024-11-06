d1a105d8ca0e8484bfa484ca59bcb4c7f7d9b358
Pavel <you@example.com>
2024-11-06 13:16:10 +0300

    feat: replace data ferching into separate function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
 import { type ReactElement } from 'react';
 
 import PageController from '@/components/PageController/PageController';
-import { ENDPOINTS } from '@/constants/const-endpoints';
-import type { TItem } from '@/types/types';
-
-const dataFetching = async (): Promise<TItem[]> => {
-  const output: TItem[] = [];
-
-  ENDPOINTS.forEach(async (endpoint) => {
-    const data = await fetch(endpoint).then((resp) => {
-      return resp.json();
-    });
-
-    output.push(...data);
-  });
-
-  return output;
-};
+import { dataFetching } from '@/services/dataFerching';
 
 const App = async (useSearchParams: { searchParams: { filter: string } }): Promise<ReactElement> => {
   const data = await dataFetching();
