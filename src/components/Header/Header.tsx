51d50ece2681c3991245612860566073b91f6783
Pavel <you@example.com>
2024-11-06 12:58:23 +0300

    feat: add link to logo in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@nextui-org/react';
 import { type ReactElement } from 'react';
 
 import { Logo } from '@/icons/Logo';
@@ -6,7 +7,9 @@ const Header = (): ReactElement => {
   return (
     <header className="h-[108px] bg-[#010C13] flex items-center text-white px-[219px]">
       <div className="flex items-center gap-5">
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
         <div className="w-[2px] h-[68px] bg-white"></div>
         <h2 className="leading-6 tracking-[5%] font-normal">Официальный дилер Максимум </h2>
       </div>
