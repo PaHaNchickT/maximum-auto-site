d7e156a24148d759e86a8da291fe009d26a30661
Pavel <you@example.com>
2024-11-08 00:55:36 +0300

    fix: fix error with types error in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ const Header = (): ReactElement => {
     <header className="h-[50px] bg-[#010C13] flex items-center text-white px-[7px] md:h-[108px] md:px-[116px] 2xl:px-[216px]">
       <div className="flex items-center gap-4 md:gap-5">
         <Link href="/">
-          <Logo width={windowWidth === 'md' ? 154 : 111} height={windowWidth === 'md' ? 42 : 28} />
+          <Logo width={windowWidth >= 768 ? 154 : 111} height={windowWidth >= 768 ? 42 : 28} />
         </Link>
         <div className="w-[3px] h-[25px] md:w-[2px] md:h-[68px] bg-white"></div>
         <h2 className="leading-4 font-normal tracking-[0.03em] text-[10px] md:leading-6 md:tracking-[0.055em] md:text-base">
