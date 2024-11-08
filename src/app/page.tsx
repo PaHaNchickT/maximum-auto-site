1d8cb8798fc806fc0ef462e15dded5bbda08e5c4
Pavel <you@example.com>
2024-11-08 04:02:05 +0300

    fix: solve problem with summary data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { type ReactElement } from 'react';
 
 import MainPageController from '@/components/MainPageController/MainPageController';
-import { dataFetching } from '@/services/dataFerching';
+import { dataFetching } from '@/services/dataFetching';
 
 const App = async (useSearchParams: { searchParams: { filter: string } }): Promise<ReactElement> => {
   const data = await dataFetching();
 
   return <MainPageController data={data} query={useSearchParams.searchParams.filter} />;
 };
-
+rzdxrxdrdr
 export default App;
--- src/services/dataFerching.ts
+++ src/services/dataFerching.ts
@@ -1,19 +1,37 @@
 'use server';
 
-import { ENDPOINTS } from '@/constants/const-endpoints';
 import type { TItem } from '@/types/types';
 
 export const dataFetching = async (): Promise<TItem[]> => {
   const output: TItem[] = [];
 
-  ENDPOINTS.forEach(async (endpoint) => {
-    const data = await fetch(endpoint).then((resp) => {
-      return resp.json();
-    });
+  const dataCherry = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Chery').then((resp) => {
+    return resp.json();
+  });
+  const dataHaval = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Haval').then((resp) => {
+    return resp.json();
+  });
+  const dataGeely = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Geely').then((resp) => {
+    return resp.json();
+  });
+  const dataExeed = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Exeed').then((resp) => {
+    return resp.json();
+  });
+  const dataOmoda = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Omoda').then((resp) => {
+    return resp.json();
+  });
+  const dataChangan = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Changan').then((resp) => {
+    return resp.json();
+  });
+  const dataJaecoo = await fetch('https://test2.maximum-haval.ru/public/test-task/v1/brand/Jaecoo').then((resp) => {
+    return resp.json();
+  });
 
-    output.push(...data);
+  Promise.all([dataCherry, dataHaval, dataGeely, dataExeed, dataOmoda, dataChangan, dataJaecoo]).then((carsData) => {
+    carsData.forEach((carsArray) => {
+      output.push(...carsArray);
+    });
   });
 
   return output;
 };
-
